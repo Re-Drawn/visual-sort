bc046b0cf5305e48f2664c691cf68940f4421977
ReDrawn <54480926+Re-Drawn@users.noreply.github.com>
2024-03-21 03:44:07 -0400

    feat: array size slider, selection sort visualization

--- algorithms/selection_sort.py
+++ algorithms/selection_sort.py
@@ -1,12 +1,16 @@
 def selection_sort(unsorted: list[int]):
+    comparisons = []
+    swaps = []
     arr = unsorted
     
     for i in range(len(arr)):
         min_num_idx = i
         for j in range(i, len(arr)):
+            comparisons.append((j, min_num_idx))
             if arr[min_num_idx] > arr[j]:
                 min_num_idx = j
-        
+        comparisons.append((i, min_num_idx))
+        swaps.append((i, min_num_idx))
         arr[i], arr[min_num_idx] = arr[min_num_idx], arr[i]
 
-    return arr
+    return arr, comparisons, swaps
--- main.py
+++ main.py
@@ -14,7 +14,7 @@ class ArrElement(pygame.sprite.Sprite):
     def __init__(self, value):
         super(ArrElement, self).__init__()
         self.value = value
-        self.surf = pygame.Surface((15, self.value*7))
+        self.surf = pygame.Surface((1600/num_elements, self.value))
         self.surf.fill((255,255,255))
         self.rect = self.surf.get_rect()
 
@@ -27,16 +27,15 @@ class ArrElement(pygame.sprite.Sprite):
     
     @staticmethod
     def clear():
-        for i in ArrElement.arr:
-            del i
+        ArrElement.arr = []
+        ArrElement.element_pos = []
 
 
-def update_display(elements = None):
+def update_display():
     display.fill((0,0,0))
 
-    if not elements:
-        for i in range(len(ArrElement.element_pos)):
-            display.blit(ArrElement.element_pos[i].surf, (i*16, 900-ArrElement.element_pos[i].value*7))
+    for i in range(len(ArrElement.element_pos)):
+        display.blit(ArrElement.element_pos[i].surf, (1600/num_elements*i, 900-ArrElement.element_pos[i].value))
     
     pygame_widgets.update(pygame.event.get())
     pygame.display.flip()
@@ -47,7 +46,8 @@ def sort():
     for i in ArrElement.element_pos:
         int_arr.append(i.value)
 
-    result, comparisons, swaps = bubble_sort(int_arr)
+    result, comparisons, swaps = selection_sort(int_arr)
+    comparison_count = 0
     swap_count = 0
 
     for comparison in comparisons:
@@ -69,9 +69,12 @@ DISPLAY_HEIGHT = 900
 display = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
 clock = pygame.time.Clock()
 shuffle_button = Button(display, 1500, 0, 100, 70, text = 'Shuffle', onClick = ArrElement.shuffle)
-sort_button = Button(display, 1400, 0, 100, 70, text='Sort', onClick = sort)
+sort_button = Button(display, 1400, 0, 100, 70, text = 'Sort', onClick = sort)
+num_elements_slider = Slider(display, 0, 0, 800, 25, min = 10, max = 800, step = 10, initial = 100)
+num_elements = num_elements_slider.value
 
-for i in range(1, 101):
+
+for i in range(1, num_elements+1):
     ArrElement(i)
 
 running = True
@@ -83,6 +86,12 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    if num_elements != num_elements_slider.value:
+        num_elements = num_elements_slider.value
+        ArrElement.clear()
+        for i in range(1, num_elements+1):
+            ArrElement(i)
 
     update_display()
 
